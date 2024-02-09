3594613adc1f858424f4575be105aa0700c2bad9
calebestes <estes.caleb.b@gmail.com>
2024-02-09 09:07:37 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission_5_Estes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿
+document.getElementById("Hours").addEventListener("keypress", function (event) {
+    // Check if the key pressed is the enter key (key code 13)
+    if (event.keyCode === 13) {
+        // Prevent the default form submission
+        event.preventDefault();
+
+        // Call the calculate function to calculate the result
+        calculateResult();
+    }
+});
+
+// Add event listener for "click" event on the "Get Quote" button
+document.getElementById("Calculate").addEventListener("click", function (event) {
+    // Prevent the default form submission
+    event.preventDefault();
+
+    // Call the calculate function to calculate the result
+    calculateResult();
+});
+
+document.getElementById("Calculate").addEventListener("click", function (event) {
+    var hours = document.getElementById("Hours").value;
+    var dollars = hours * 10; // Assuming $10 per hour
+    document.getElementById("result").value = dollars
+
+});
+
+
